c70714bc3e9576257c1710ae8a54b5ece6853a59
Valeriia-Livinska <valerylivi@gmail.com>
2023-02-28 01:54:17 +0100

    fix updating avatar url

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,31 +1,43 @@
+const { User } = require("../../models/user");
+
 const fs = require("fs/promises");
 const path = require("path");
 const jimp = require("jimp");
+const { Unauthorized } = require("http-errors");
 
-const updateAvatar = async (req, res, next) => {
-  const { path: oldPath, filename } = req.file;
+const newPath = path.join(__dirname, "../../", "public", "avatars");
+
+const updateAvatar = async (req, res) => {
+  const { path: oldPath, originalname } = req.file;
   const { _id: userId } = req.user;
-  const newPath = path.join(__dirname, "../../", "public", "avatars");
-  try {
-    const [extension] = filename.split(".").reverse();
-    const newAvatarName = `${userId}.${extension}`;
 
-    const avatarImg = await jimp.read(oldPath);
-    avatarImg.autocrop().cover(250, 250, jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE).writeAsync(oldPath);
+  try {
+    const [extension] = originalname.split(".").reverse();
+    const newAvatarName = `${userId}.${extension}`;
 
     const newAvatarPath = path.join(newPath, newAvatarName);
     await fs.rename(oldPath, newAvatarPath);
 
+    const avatarImg = await jimp.read(newAvatarPath);
+    await avatarImg
+      .autocrop()
+      .cover(
+        250,
+        250,
+        jimp.HORIZONTAL_ALIGN_CENTER || jimp.VERTICAL_ALIGN_MIDDLE
+      )
+      .write(newAvatarPath);
+
     const newAvatarUrl = path.join("avatars", newAvatarName);
+
+    await User.findByIdAndUpdate(userId, { avatarURL: newAvatarUrl });
 
     res.status(200).json({
       avatarURL: newAvatarUrl,
     });
   } catch (error) {
-    fs.unlink(oldPath);
-    res.status(401).json({
-      message: "Not authorized",
-    });
+    await fs.unlink(oldPath);
+    throw new Unauthorized("Not authorized");
   }
 };
 
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -3,16 +3,16 @@ const path = require("path");
 
 const tempDir = path.join(__dirname, "../", "tmp");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+const multerConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
     cb(null, tempDir);
   },
-  filename: function (req, file, cb) {
+  filename: (req, file, cb) => {
     const { originalname } = file;
     cb(null, originalname);
   },
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: multerConfig });
 
 module.exports = upload;
